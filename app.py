08153a024c95ae51210ad76b027dba4de06416c7
debra-soth <s_soth23@stud.hwr-berlin.de>
2024-11-25 12:05:31 +0100

    Update db.py and app.py files

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
+import os
 from flask import Flask
+import db
 
 app = Flask(__name__)
 
+app.config.from_mapping(
+    SECRET_KEY='secret_key_just_for_dev_environment',
+    DATABASE=os.path.join(app.instance_path, 'todos.sqlite')
+)
+app.cli.add_command(db.init_db)
+app.teardown_appcontext(db.close_db_con)
+
 @app.route('/')
 def index():
-    return 'Test!'
+    return 'Hello, World!'
