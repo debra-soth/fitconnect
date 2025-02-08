e8971c3caf264b8880a84af4c147bfe6a30a4481
lenjaKrah <s_krah23@stud.hwr-berlin.de>
2025-02-08 18:43:33 +0100

    personalizeProfile.html: UI Screen fürd das Personalisieren des Profils erstellen und alle Komponenten hinzufügen

--- app.py
+++ app.py
@@ -4,11 +4,15 @@ app = app = Flask(__name__)
 
 @app.route('/login')
 def index():
-    # login.html template rendern 
+#Route für login.html
     return render_template('login.html')
+#Route für register.html
 @app.route('/register')
 def register():
     return render_template('register.html')
-
+#Route für personalizeProfile.html
+@app.route('/personalize')
+def personalize_profile():
+    return render_template('personalizeProfile.html')
 if __name__ == '__main__':
     app.run(debug=True)
