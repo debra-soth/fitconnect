a20e41661b697658b43c727d940ad28c4ddd605a
lenjaKrah <s_krah23@stud.hwr-berlin.de>
2025-02-10 18:16:14 +0100

    Route für das Personalisieren des Profils erstellen in auth.py, damit das Profil gespeichert wird, wenn der Benutzer das Formular abschickt

--- auth.py
+++ auth.py
@@ -6,7 +6,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, current_user, login_required
 
 # User-Modell, Registrieruns-Forms und DB-Verbindung werden importiert
-from fitconnect.db import db
+from .db import db
 from .models import User
 from .forms import RegistrationForm
 
@@ -81,3 +81,38 @@ def logout():
     # Gib eine Erfolgsmeldung aus und leite Benutzer zur Login Seite weiter.
     flash('Successfully logged out!', category='success')
     return redirect(url_for('auth.login'))
+
+# Route für das Personalisieren des Profils
+@auth.route('/personalize', methods=['GET', 'POST'])
+@login_required
+def personalize_profile():
+    form = PersonalizeProfileForm()
+    
+    # Wenn das Formular abgeschickt wird (POST)
+    if form.validate_on_submit():
+        # Profilbild hochladen und speichern
+        if form.profile_photo.data:
+            profile_photo = form.profile_photo.data
+            photo_filename = secure_filename(profile_photo.filename)
+            photo_path = os.path.join('static/images', photo_filename)
+            profile_photo.save(photo_path)
+        else:
+            photo_path = None
+        
+        # Benutzerinformationen aktualisieren
+        current_user.profile_photo = photo_path
+        current_user.favorite_activities = form.favorite_activity.data
+        current_user.gym_membership = form.gym_membership.data
+        current_user.availability = form.availability.data
+        current_user.fitness_level = form.fitness_level.data
+        current_user.age = form.age.data
+        current_user.gender = form.gender.data
+        current_user.motivation_text = form.motivation_text.data
+        
+        # Änderungen in der Datenbank speichern
+        db.session.commit()
+
+        flash('Your profile has been updated successfully!', 'success')
+        return redirect(url_for('auth.personalize_profile'))
+    
+    return render_template('personalizeProfile.html', form=form)
