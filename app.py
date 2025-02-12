edbb5496ce8159584b36a8bd67ba76a7fe88e8e5
lenjaKrah <s_krah23@stud.hwr-berlin.de>
2025-02-12 13:29:46 +0100

    accountSettings.html auf den neusten Stand bringen (PersonalizeProfile.hmtl als Vorlage verwenden)

--- app.py
+++ app.py
@@ -16,10 +16,11 @@ def index():
 def personalize_profile():
     return render_template('personalizeProfile.html', form=PersonalizeProfileForm())  # Übergebe das Formular an das Template
 
-#Route für accountSettings
-@app.route('/settings')
+#Route für accountSettings.html
+@app.route('/settings', methods=['GET', 'POST'])
 def account_settings():
-    return render_template('accountSettings.html')
+    form = PersonalizeProfileForm()  # Erstelle eine Instanz des Formulars
+    return render_template('accountSettings.html', form=form)  # Form ans Template übergeben
 
 #Route für userOverview.html
 @app.route('/user')
